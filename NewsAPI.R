3374401f4252e24351c477f5c19319d113fb5ef2
Dani Madrid-Morales <dmmorale@central.uh.edu>
2021-06-02 22:44:11 -0500

    annotated steps

--- NewsAPI.R
+++ NewsAPI.R
@@ -1,9 +1,8 @@
-#install.packages("newsanchor")
-library("newsanchor")
-library(readxl)
-library(data.table)
-library(tidyverse)
+library(pacman)
+p_load(newsanchor, readxl, data.table, tidyverse)
 
+
+##### Scrape data from News API using the API from January 1 to May 31 2020 #####
 
 # Generates range of alternating dates, plus one day lag
 dates <- seq.Date(as.Date("2020/1/1"), as.Date("2020/05/31"), by = 2)
@@ -16,6 +15,7 @@ rm(dates2)
 terms <- c("(Chin*)",
            "(COVID) OR (COVID-19)",
            "coronavirus")
+
 # Function to repeat the number of terms, so that they match the length of dates
 repeat_names <- function(x){
   df <- tibble(term = x) %>%
@@ -32,13 +32,14 @@ rm(dates,terms,dates2,repeat_names)
 # Function to retrieve content from Google News API
 get_news_api_all <- function(api_query, date_from, date_to){
   df <- get_everything_all(query = api_query, 
-                           api_key = "XXXX",
+                           api_key = "XXXXXX", #[to be set by the user] 
                            from = date_from,
                            to = date_to)
   df <- df[["results_df"]] %>%
     bind_rows()
 }
 
+# Split data collection process in chunks
 df.covid <- pmap(list(search_terms$term[1:75], 
                       search_terms$start_date[1:75],
                       search_terms$end_date[1:75]), 
